161742295e903ff602aaca13f009ac6ca56d6c61
Nosuke <example@gmail.com>
2022-03-07 09:27:05 +0000

    kadai lara19

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,7 @@ Route::group(['prefix' => 'admin'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
+Route::get('//', 'ProfileController@index');
 
 
